f44d3df85f823617d3434b23731b5338e15fc054
Katie Ammon <katie.ammon10@gmail.com>
2022-04-23 18:52:11 -0600

    Add intercept to cypress tests

--- cypress/integration/main-page.js
+++ cypress/integration/main-page.js
@@ -1,29 +1,46 @@
 describe('Main Page flow', () => {
 
+     let interceptData;
+
+     beforeEach(() => {
+          interceptData = {
+               "activity":"Pot some plants and put them around your house",
+             "type":"relaxation",
+             "participants":1,
+             "price":0.4,
+             "link":"",
+             "key":"6613330",
+             "accessibility":0.3}  
+          })
+
      it('should display a header and form', () => {
+          cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
           cy.visit('http://localhost:3000/')
           .get('.header').contains('logo')
           .get('.form').contains('Choose your catagory!')
      })
 
      it('a user should be able to select and submit a catagory and card should pop up with an idea', () => {
-          cy.visit('http://localhost:3000/')
-          .get('.form').get('.catagory').select('music')
+          cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
+          cy.visit('http://localhost:3000/social')
+          .get('.form').get('.catagory').select('all')
           .get('.submit').click()
           .get('.primaryCard').should('be.visible')
      })
 
      it('should only allow a user to select the challange once', () => {
-          cy.visit('http://localhost:3000/')
-          .get('.form').get('.catagory').select('social')
+          cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
+          cy.visit('http://localhost:3000/social')
+          .get('.catagory').select('all')
           .get('.submit').click()
           .get('.primaryCard').get('.addChallenge').click()
           .get('.primaryCard').get('.addChallenge').should('not.exist')
      })
 
      it('A user should be able to click on the Accepted Challenges button to see accepted challenges cards', () => {
+          cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
           cy.visit('http://localhost:3000/')
-          .get('.form').get('.catagory').select('music')
+          .get('.form').get('.catagory').select('all')
           .get('.submit').click()
           .get('.primaryCard').get('.addChallenge').click()
           .get('.accepted-challenges').click()
@@ -31,13 +48,15 @@ describe('Main Page flow', () => {
      })
 
      it('A user should be able to choose another challange', () => {
+            cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
            cy.visit('http://localhost:3000/')
-          .get('.form').get('.catagory').select('music')
+          .get('.form').get('.catagory').select('all')
           .get('.submit').click()
           .get('.primaryCard').get('.newChallenge').click()
      })
 
      it('A user should be able to click on the Completed Challenges button to see all completed challenges', () => {
+            cy.intercept('GET', 'http://https://www.boredapi.com/api/activity/', interceptData)
            cy.visit('http://localhost:3000/')
            .get('.completed-challenges').click()
            .get('.cardbox-header').contains('your Completed Challenges')
