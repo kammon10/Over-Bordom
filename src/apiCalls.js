a0844e7bd87fa0dae8d1549df8708e8784e8e58e
Katie Ammon <katie.ammon10@gmail.com>
2022-04-24 14:44:14 -0600

    Refactor API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,14 @@
 const getData = () => {
      return fetch('https://www.boredapi.com/api/activity')
-     .then(response => response.json())
-     .catch(err => console.log(err))
+          .then(response => response.json())
+          .catch(err => console.log(err))
 
 }
 
 const getDataByType = (type) => {
      return fetch(`https://www.boredapi.com/api/activity?type=${type}`)
-     .then(response => response.json())
-     .catch(err => console.log(err))
+          .then(response => response.json())
+          .catch(err => console.log(err))
 }
 
-export {getData, getDataByType}
+export { getData, getDataByType }
