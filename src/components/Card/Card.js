86fdd38de53211bc9d3f0b732b9314f8aa339841
Katie Ammon <katie.ammon10@gmail.com>
2022-04-24 14:43:20 -0600

    Refactor

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,12 @@
 import './Card.css'
 
-const Card = ({activity, type, removeIdea, completeIdea, style}) => 
-{
-     return(
+const Card = ({ activity, type, removeIdea, completeIdea, style }) => {
+     return (
           <div className='card'>
                <h2>{activity}</h2>
                <p>{type}</p>
                <div>
-                    { style === 'acceptedChallenges' && <><button className='remove-idea-btn' onClick={() => removeIdea({ activity })}>Delete</button><button className='complete-idea-btn' onClick={() => completeIdea({ activity})}>Complete</button></>}
+                    {style === 'acceptedChallenges' && <><button className='remove-idea-btn' onClick={() => removeIdea({ activity })}>Delete</button><button className='complete-idea-btn' onClick={() => completeIdea({ activity })}>Complete</button></>}
                </div>
           </div>
      )
--- src/components/CardBox/CardBox.js
+++ src/components/CardBox/CardBox.js
@@ -1,24 +1,24 @@
 import Card from '../Card/Card'
 import './CardBox.css'
 
-const CardBox = ({ideas, removeIdea, completeIdea, type, message}) => {
-     const allIdeas = ideas.map(idea => 
-     <Card 
-          key={idea.key}
-          activity={idea.activity}
-          type={idea.type}
-          removeIdea={removeIdea}
-          completeIdea={completeIdea}
-          style={type}
-     />
-    )
-     return(
+const CardBox = ({ ideas, removeIdea, completeIdea, type, message }) => {
+     const allIdeas = ideas.map(idea =>
+          <Card
+               key={idea.key}
+               activity={idea.activity}
+               type={idea.type}
+               removeIdea={removeIdea}
+               completeIdea={completeIdea}
+               style={type}
+          />
+     )
+     return (
           <section className='cardbox-section'>
                <div className='cardbox-header'>
-               <h1>{message}</h1>
+                    <h1>{message}</h1>
                </div>
                <div className='card-housing'>
-               {allIdeas}
+                    {allIdeas}
                </div>
           </section>
      )
