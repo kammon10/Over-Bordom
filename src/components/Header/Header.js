3074ae1b610ea4773c6979d927dbda865d9edf2b
Katie Ammon <katie.ammon10@gmail.com>
2022-04-24 14:40:14 -0600

    Refactor Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,22 @@
-import { NavLink, Link, useLocation } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import './Header.css'
 
 const Header = () => {
-     const location = useLocation()
 
-     return(
+     return (
           <section className='header'>
-               <NavLink className='logo'to='/'>
+               <NavLink className='logo' to='/'>
                     <h1 className='logo'>Over Bordom</h1>
                </NavLink>
                <div className='home'>
                </div>
                <div className='links'>
-                 <Link to='/acceptedChallenges'>
-                    <button className='accepted-challenges'>Accepted Ideas</button>
-                    </Link> 
-                         <Link to='/completedIdeas'>
+                    <Link to='/acceptedChallenges'>
+                         <button className='accepted-challenges'>Accepted Ideas</button>
+                    </Link>
+                    <Link to='/completedIdeas'>
                          <button className='completed-challenges' >Completed Ideas</button>
-                    </Link>   
+                    </Link>
                </div>
           </section>
      )
